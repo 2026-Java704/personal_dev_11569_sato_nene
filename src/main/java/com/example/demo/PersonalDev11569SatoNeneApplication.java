befcb9c810070839ca97044a58885905076adaa1
student <student@FJ-1025>
2026-05-19 12:11:56 +0900

    新規

---
+++ src/main/java/com/example/demo/PersonalDev11569SatoNeneApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev11569SatoNeneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev11569SatoNeneApplication.class, args);
+	}
+
+}
